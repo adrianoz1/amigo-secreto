906e411ddee8818a0211608686c362cd8804b348
adrianoz1 <adriano@z1.app>
2024-11-01 22:18:23 -0300

    chore: log test env

--- src/app/api/sortear/route.ts
+++ src/app/api/sortear/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+console.log("API Key:", process.env.RESEND_API_KEY); // Remover após teste
 
 interface Participante {
   nome: string;
